549230bdae0fd8d476c51e31a02f29b6c21fd712
TaufikQurohman <taufikqurohman@student.telkomuniversity.ac.id>
2025-11-24 13:58:02 +0700

    update main page content with date and create review page

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
-        ini page utama updates diupdate senin 24/11/2025
+        diupdate senin 24/11/2025
       </main>
     </div>
   );
