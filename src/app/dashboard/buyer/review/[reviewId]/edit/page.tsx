c89b124407326abe3e84420f28ca5b1eb9ae59c9
TaufikQurohman <taufikqurohman@student.telkomuniversity.ac.id>
2025-12-22 07:37:17 +0700

    feat-review-edit-delete-new

--- src/app/dashboard/buyer/review/[reviewId]/edit/page.tsx
+++ src/app/dashboard/buyer/review/[reviewId]/edit/page.tsx
@@ -161,7 +161,7 @@ export default function EditReviewPage() {
             >
               Simpan Perubahan
             </Button>
-            
+
 
             <AlertDialog>
               <AlertDialogTrigger asChild>
